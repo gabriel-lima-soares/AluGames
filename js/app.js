f5221565054524762f766b9368d138f88ec05575
Gabriel Soares <gabriel.lima.soares07@gmail.com>
2024-06-24 16:38:13 -0400

    Adicionado Confirmação ao Devolver o Jogo

--- js/app.js
+++ js/app.js
@@ -2,13 +2,15 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
        
     if(imagem.classList.contains('dashboard__item__img--rented')){
-
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.textContent = 'Alugar';
-        botao.classList.remove('dashboard__item__button--return');    
-    
+        //Adicionado a Confirmação
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.textContent = 'Alugar';
+            botao.classList.remove('dashboard__item__button--return');    
+        }
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver';
